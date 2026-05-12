987101513ea46913b81321c3b5e156e31d5b8cc8
BWooRam <wrbae@hyundaiht.co.kr>
2026-05-12 09:29:54 +0900

    Test Coverage를 위한 테스트 코드 추가

--- app/src/test/java/com/hyundaiht/proguardtest/ui/data/DataClassesTest.kt
+++ app/src/test/java/com/hyundaiht/proguardtest/ui/data/DataClassesTest.kt
@@ -46,4 +46,60 @@ class DataClassesTest {
         assertEquals("Test4", test2.name)
         assertEquals(0, test2.age)
     }
+
+    @Test
+    fun testTest3Creation() {
+        val test3 = Test3(name = "Test3", age = 30)
+        assertEquals("Test3", test3.name)
+        assertEquals(30, test3.age)
+    }
+
+    @Test
+    fun testTest3DefaultAge() {
+        val test3 = Test3(name = "Test4")
+        assertEquals("Test4", test3.name)
+        assertEquals(0, test3.age)
+    }
+
+    @Test
+    fun testTest4Creation() {
+        val test4 = Test4(name = "Test4", age = 40)
+        assertEquals("Test4", test4.name)
+        assertEquals(40, test4.age)
+    }
+
+    @Test
+    fun testTest4DefaultAge() {
+        val test4 = Test4(name = "Test4")
+        assertEquals("Test4", test4.name)
+        assertEquals(0, test4.age)
+    }
+
+    @Test
+    fun testTest5Creation() {
+        val test5 = Test5(name = "Test5", age = 50)
+        assertEquals("Test5", test5.name)
+        assertEquals(50, test5.age)
+    }
+
+    @Test
+    fun testTest5DefaultAge() {
+        val test5 = Test5(name = "Test5")
+        assertEquals("Test5", test5.name)
+        assertEquals(0, test5.age)
+    }
+
+    @Test
+    fun testTest6Creation() {
+        val test6 = Test6(name = "Test6", age = 60)
+        assertEquals("Test6", test6.name)
+        assertEquals(60, test6.age)
+    }
+
+    @Test
+    fun testTest6DefaultAge() {
+        val test6 = Test6(name = "Test6")
+        assertEquals("Test6", test6.name)
+        assertEquals(0, test6.age)
+    }
 }
